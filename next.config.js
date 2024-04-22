79e07c8276d8d792ecbb870fdc22e040207258ac
Gordon Lee <gordonlee@Gordonui-MacBookAir.local>
2024-04-22 14:43:06 +0900

    feat config redirection set-up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        // UI input url
+        source: '/old-url/:path*',
+        // redirected url
+        destination: "/new-url/:path*",
+        permanent: false,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
