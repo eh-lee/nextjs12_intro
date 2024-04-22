ff4f18b7fb51998819e00b0349cce5e9471e6153
Gordon Lee <gordonlee@Gordonui-MacBookAir.local>
2024-04-22 15:47:18 +0900

    Feat: index.js getServerSideProps set-up

--- pages/index.js
+++ pages/index.js
@@ -4,42 +4,37 @@ import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 import Link from "next/link";
 
-function index() {
+// function Home() {
+  // serverSideProps 받는 경우 아래와 같이 파라미터 전달,
+  function Home({ movies }) {
 
-  const [movies, setMovies] = useState([]);
+  // const [movies, setMovies] = useState([]);
 
- useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/movies", {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDI0YjY2MGMyNDMxZThmNDNiNDVmNzQzYzNjMDQyYyIsInN1YiI6IjY2MjVjNjY0MjU4ODIzMDE2NDkxMmVmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q1-1A2mU2qg13n561yqjy-JGuyFp2evDX9Wd8YUklZk'
-          }
-        });
-        const data = await response.json();
-        console.log("data------>", data)
-        setMovies(data.results);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
-  
-  // useEffect(()=>{
-  //   console.log("movies----->", movies);
-  // }, [movies])
-
-
+//  useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch("/api/movies", {
+//           method: 'GET',
+//           headers: {
+//             accept: 'application/json',
+//             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDI0YjY2MGMyNDMxZThmNDNiNDVmNzQzYzNjMDQyYyIsInN1YiI6IjY2MjVjNjY0MjU4ODIzMDE2NDkxMmVmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q1-1A2mU2qg13n561yqjy-JGuyFp2evDX9Wd8YUklZk'
+//           }
+//         });
+//         const data = await response.json();
+//         console.log("data------>", data)
+//         setMovies(data.results);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     fetchData();
+//   }, []);
 
   return (
     <div className="container">
       <Seo title="Home" />
       {movies?.map((movie) => (
         <div
-          // onClick={() => onClick(movie.id, movie.original_title)}
           className="movie"
           key={movie.id}
         >
@@ -79,4 +74,33 @@ function index() {
   );
 }
 
-export default index;
+export default Home;
+
+// 아래 예시 펑션은 only server side에서만 실행되는 코드
+// API_KEY 같은 거 client에서 보이지 않음
+// 펑션명은 고정 'getServerSideProps'
+export async function getServerSideProps() {
+  try {
+    const response = await fetch("http://localhost:3000/api/movies", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDI0YjY2MGMyNDMxZThmNDNiNDVmNzQzYzNjMDQyYyIsInN1YiI6IjY2MjVjNjY0MjU4ODIzMDE2NDkxMmVmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q1-1A2mU2qg13n561yqjy-JGuyFp2evDX9Wd8YUklZk",
+      },
+    });
+    const data = await response.json();
+    const movies = data.results;
+    return {
+      props: {
+        movies,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        movies: [],
+      },
+    };
+  }
+}
