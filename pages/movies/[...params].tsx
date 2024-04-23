7573ed4def45e0a5a13290f19cd3e6e3266fd9d7
Gordon Lee <gordonlee@Gordonui-MacBookAir.local>
2024-04-23 16:21:16 +0900

    Fix: Dynamic URL ServerProps Handling ISSUE Solved [...params].tsx

--- pages/movies/[...params].tsx
+++ pages/movies/[...params].tsx
@@ -1,5 +1,7 @@
-// 수정 필요
-// ISSUE: serverProps를 제대로 못 받아옴
+// ************** ver.2 *************
+// ServerProps params 받아와서 Client에서 렌더링
+// TypeScript
+// ************** ver.2 *************
 import { useRouter } from "next/router";
 import Seo from "../../components/Seo";
 import { GetServerSideProps } from "next";
@@ -22,7 +24,9 @@ export default function Detail({ title }: DetailProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const { title, id } = params;
+  const paramsArray = params?.params as string[] | undefined;
+  const title = paramsArray ? paramsArray[0] : undefined;
+  const id = paramsArray ? parseInt(paramsArray[1], 10) : undefined;
 
   // 예외 처리
   const safeTitle = title ?? "Unknown Title";
@@ -39,7 +43,34 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 };
 
 
-// js
+// ************** ver.3 *************
+// ServerProps 없이
+// Client에서만 title 핸들링
+// ************** ver.3 *************
+
+// import { useRouter } from 'next/router';
+// import Seo from '../../components/Seo';
+
+// export default function Detail() {
+//   const router = useRouter();
+//   console.log("router==>", router.query);
+//   const params = router.query.params as string[] | undefined;
+//   const title = params ? params[0] : undefined;
+
+//   return (
+//     <>
+//       <Seo title={title} />
+//       <h4>{title || 'Loading ... '}</h4>
+//     </>
+//   );
+// }
+
+
+
+// ************** ver.1 *************
+// ServerProps params 받아와서 Client에서 렌더링
+// JavaScript
+// ************** ver.1 *************
 
 // import { useRouter } from "next/router";
 // import Seo from "../../components/Seo";
