b0a102198f40dfec23166c400b7e493655e0b483
Gordon Lee <gordonlee@Gordonui-MacBookAir.local>
2024-04-24 10:53:21 +0900

    Fix: next.config.js/ts -ing commit for checkout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const TMDB_API_KEY: string | undefined = process.env.TMDB_API_KEY;
+// const TMDB_API_KEY: string | undefined = process.env.TMDB_API_KEY;
 // "string | undefined" => value가 undefined인 경우 예외 처리를 위함
+
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 const nextConfig = {
   reactStrictMode: true,
